536de8fedbf138f5567996d6d2efb4a5303b5a78
Ignacio Velderrain <nasho_shinoda_1988@hotmail.com>
2021-06-19 13:45:02 -0700

    Update homepage.py

--- pages/homepage.py
+++ pages/homepage.py
@@ -7,12 +7,12 @@ screenshot_date = date_time.strftime("%m%d%Y-%H%M%S")
 class HomePage:
     def form_authentication(self, browser):
         form_link = browser.find_element_by_css_selector('a[href="/login"]').click()
-    
+
     def fill_login_form(self, browser, username, password):
         username_field = browser.find_element_by_id('username').send_keys(username)
         password_field = browser.find_element_by_id('password').send_keys(password)
         login_btn      = browser.find_element_by_css_selector('button[type="submit"]').click()
-        
+
     def messages(self, browser):
         message_box        = browser.find_element_by_id('flash').get_attribute('class')
         msg_box_screenshot = browser.find_element_by_id('flash')
