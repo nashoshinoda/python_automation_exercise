b015f96f905bb0d372370e9ccef78e7c7c402eac
Ignacio Velderrain <nasho_shinoda_1988@hotmail.com>
2021-06-19 14:45:30 -0700

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,8 +4,8 @@ from pages.homepage import HomePage
 class LoginTests:
     def test_login_with_invalid_data(self, browser, invalid_test_data):
         browser.get("https://the-internet.herokuapp.com/")
-        HomePage.form_authentication('', browser)
-        HomePage.fill_login_form('', browser, invalid_test_data["username"], invalid_test_data["password"])
+        HomePage.form_authentication(self, browser)
+        HomePage.fill_login_form(self, browser, invalid_test_data["username"], invalid_test_data["password"])
 
         if HomePage.messages(self, browser) == "Failure":
             logging.info("You used invalid credentials")
@@ -15,8 +15,8 @@ class LoginTests:
 
     def test_login_with_valid_data(self, browser, valid_test_data):
         browser.get("https://the-internet.herokuapp.com/")
-        HomePage.form_authentication('', browser)
-        HomePage.fill_login_form('', browser, valid_test_data["username"], valid_test_data["password"])
+        HomePage.form_authentication(self, browser)
+        HomePage.fill_login_form(self, browser, valid_test_data["username"], valid_test_data["password"])
 
         if HomePage.messages(self, browser) == "Success":
             logging.info("You used valid credentials")
