971ba44a8dd06b1e166c20bf560ea925cfe86231
Ignacio Velderrain <nasho_shinoda_1988@hotmail.com>
2021-06-19 13:43:46 -0700

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,16 +6,20 @@ class LoginTests:
         browser.get("https://the-internet.herokuapp.com/")
         HomePage.form_authentication('', browser)
         HomePage.fill_login_form('', browser, invalid_test_data["username"], invalid_test_data["password"])
-        
-        if HomePage.messages == "Failure":
+
+        if HomePage.messages(self, browser) == "Failure":
             logging.info("You used invalid credentials")
-            assert "invalid" in HomePage.messages
+            browser.implicitly_wait(10)
+
+            assert "Failure" in HomePage.messages(self, browser)
 
     def test_login_with_valid_data(self, browser, valid_test_data):
         browser.get("https://the-internet.herokuapp.com/")
         HomePage.form_authentication('', browser)
         HomePage.fill_login_form('', browser, valid_test_data["username"], valid_test_data["password"])
-        
-        if HomePage.messages == "Success":
+
+        if HomePage.messages(self, browser) == "Success":
             logging.info("You used valid credentials")
-            assert "logged" in HomePage.messages
+            browser.implicitly_wait(10)
+
+            assert "Success" in HomePage.messages(self, browser)
